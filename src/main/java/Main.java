0bbb00824b3e750b28acc28cfa6478a17f776383
bezrukov <bezrukov-yra@mail.ru>
2021-05-29 11:51:16 +0300

    my second commit in task1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import org.apache.commons.lang3.ArrayUtils;
 import java.util.*;
 
 public class Main {
-    
+
 
 
 
@@ -14,11 +14,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < 100; i++) {
-            set.add(1);
-        }
-        System.out.println(set.size());
         System.out.println("-|-|-|-|-|-|-|-|-|-|-|-|");
         System.out.println("-|-|-|-|-|-|-|-|-|-|-|-|");
         System.out.println("-|-|-|-|-|-|-|-|-|-|-|-|");
